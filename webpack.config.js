44040f6a1d0d946c4b6cdd3fb809d4fb4071d3e9
KozyaSkyworker <d_koz@inbox.ru>
2023-12-29 10:20:23 +0400

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src', 'index.tsx'), // точка входа
+  // выход
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'), // где index.html
+    },
+    port: 3000,
+    compress: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.jsx'],
+  },
+};
